85a1f5c063bd95c8bf0778eaa9a21c8075e72c58
Victor Suetnov <v.suetnov@citrus-soft.ru>
2024-02-07 16:00:42 +0300

    added a comprehensive news component for reviews

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -12,21 +12,24 @@ use Bitrix\Main\Localization\Loc;
 
     <!-- side menu -->
     <?$APPLICATION->IncludeComponent(
-        "bitrix:menu",
-        "left",
-        Array(
-            "ALLOW_MULTI_SELECT" => "N",
-            "CHILD_MENU_TYPE" => "left",
-            "DELAY" => "N",
-            "MAX_LEVEL" => "1",
-            "MENU_CACHE_GET_VARS" => array(0=>"",),
-            "MENU_CACHE_TIME" => "3600",
-            "MENU_CACHE_TYPE" => "N",
-            "MENU_CACHE_USE_GROUPS" => "Y",
-            "ROOT_MENU_TYPE" => "left",
-            "USE_EXT" => "Y"
-        )
-    );?>
+	"bitrix:menu", 
+	"left", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "left",
+		"USE_EXT" => "Y",
+		"COMPONENT_TEMPLATE" => "left"
+	),
+	false
+);?>
     <!-- /side menu -->
 
     <!-- side anonse -->
