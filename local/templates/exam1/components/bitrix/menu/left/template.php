35303ccac2eaf44ef026b67a317f0ae287b3d789
Victor Suetnov <v.suetnov@citrus-soft.ru>
2024-02-07 12:39:18 +0300

    added a left menu

--- local/templates/exam1/components/bitrix/menu/left/template.php
+++ local/templates/exam1/components/bitrix/menu/left/template.php
@@ -1,20 +1,22 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
 <?if (!empty($arResult)):?>
-<ul class="left-menu">
+    <div class="side-block side-menu">
+        <div class="title-block"><?=GetMessage("T_EXAM_MENU_TITLE")?></div>
+        <div class="menu-block">
+            <ul>
+                <?
+                foreach($arResult as $arItem):
+                    if($arItem["DEPTH_LEVEL"] > 1)
+                        continue;
+                    ?>
+                    <?if($arItem["SELECTED"]):?>
+                    <li class="selected"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <?else:?>
+                    <li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+                <?endif?>
 
-<?
-foreach($arResult as $arItem):
-	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
-		continue;
-?>
-	<?if($arItem["SELECTED"]):?>
-		<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
-	<?else:?>
-		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-	<?endif?>
-	
-<?endforeach?>
-
-</ul>
+                <?endforeach?>
+            </ul>
+        </div>
+    </div>
 <?endif?>
